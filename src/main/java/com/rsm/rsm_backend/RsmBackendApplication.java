1ae7ec3529214c7f14a9644f0e92a45bb49aee81
johnszyna <jan.szyndlarewicz@gmail.com>
2023-11-12 17:36:31 +0100

    Environment setup

---
+++ src/main/java/com/rsm/rsm_backend/RsmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rsm.rsm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsmBackendApplication.class, args);
+    }
+
+}
