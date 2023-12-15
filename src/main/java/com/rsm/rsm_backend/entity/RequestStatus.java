f68af54abc8684f6a8b6e475febeb6cb2b2ffee4
johnszyna <jan.szyndlarewicz@gmail.com>
2023-12-15 19:37:51 +0100

    Request status updated

--- src/main/java/com/rsm/rsm_backend/entity/RequestStatus.java
+++ src/main/java/com/rsm/rsm_backend/entity/RequestStatus.java
@@ -3,8 +3,10 @@ package com.rsm.rsm_backend.entity;
 public enum RequestStatus {
     WAITING_FOR_AN_EMAIL_VERIFICATION,
     WAITING_FOR_A_MECHANIC_ASSIGNMENT,
+    WAITING_FOR_DATE_ASSIGNMENT,
     WAITING_FOR_A_CAR,
     CAR_DIAGNOSIS,
     IN_REPAIR,
-    READY_TO_GO
+    READY_TO_GO,
+    DONE
 }
